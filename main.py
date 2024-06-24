198e45af0c5b9987f250df53fcf618bd02160852
atarutin <andrey.tarutin@lykke-business.ch>
2024-06-24 13:46:06 +0300

    style: code formatting according to pylint

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 """
 This script deletes all queues that match a certain pattern.
-It uses the RabbitMQ Management HTTP API to list all queues 
+It uses the RabbitMQ Management HTTP API to list all queues
 and then deletes the ones that match the pattern.
-The pattern is defined by two regular expressions, one for 
+The pattern is defined by two regular expressions, one for
 the queue name and one for the vhost.
 """
 
+import argparse
+import configparser
 import re
 
 import requests
-import argparse
-import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
-parser = argparse.ArgumentParser(description='RabbitMQ Queues Deleter by Pattern')
-parser.add_argument('--username', type=str, default=config.get('Defaults', 'username'), help='RabbitMQ admin username')
-parser.add_argument('--password', type=str, default=config.get('Defaults', 'password'), help='RabbitMQ admin password')
-parser.add_argument('--host', type=str, default=config.get('Defaults', 'host'), help='RabbitMQ host')
-parser.add_argument('--vhost', type=str, default=config.get('Defaults', 'vhost'), help='RabbitMQ vhost')
-parser.add_argument('--pattern', type=str, default=config.get('Queues', 'name_regex'), help='Queue name regular expression')
+parser = argparse.ArgumentParser(
+    description="RabbitMQ Queues Deleter by Pattern"
+)
+parser.add_argument(
+    "--username",
+    type=str,
+    default=config.get("Defaults", "username"),
+    help="RabbitMQ admin username",
+)
+parser.add_argument(
+    "--password",
+    type=str,
+    default=config.get("Defaults", "password"),
+    help="RabbitMQ admin password",
+)
+parser.add_argument(
+    "--host",
+    type=str,
+    default=config.get("Defaults", "host"),
+    help="RabbitMQ host",
+)
+parser.add_argument(
+    "--vhost",
+    type=str,
+    default=config.get("Defaults", "vhost"),
+    help="RabbitMQ vhost",
+)
+parser.add_argument(
+    "--pattern",
+    type=str,
+    default=config.get("Queues", "name_regex"),
+    help="Queue name regular expression",
+)
 
 args = parser.parse_args()
 
@@ -28,7 +55,8 @@ USERNAME = args.username
 PASSWORD = args.password
 HOST = args.host
 VHOST = args.vhost
-QUEUES_API_URL = f'{HOST}/api/queues'
+QUEUES_API_URL = f"{HOST}/api/queues"
+
 
 def delete_queue(queue_json) -> int:
     """
@@ -39,8 +67,11 @@ def delete_queue(queue_json) -> int:
     :return: The status code of the DELETE request.
     """
     delete_url = f"{QUEUES_API_URL}/{VHOST}/{queue_json['name']}"
-    delete_response = requests.delete(delete_url, auth=(USERNAME, PASSWORD), timeout=5)
+    delete_response = requests.delete(
+        delete_url, auth=(USERNAME, PASSWORD), timeout=5
+    )
     return delete_response.status_code
+
 
 def get_queues() -> list:
     """
@@ -49,25 +80,33 @@ def get_queues() -> list:
     :return: A list of JSON objects representing the queues.
     :raises Exception: If the request to the RabbitMQ Management API fails.
     """
-    response = requests.get(QUEUES_API_URL, auth=(USERNAME, PASSWORD), timeout=5)
+    response = requests.get(
+        QUEUES_API_URL, auth=(USERNAME, PASSWORD), timeout=5
+    )
     if response.status_code != 200:
-        raise Exception(f"Error getting queues: {response.status_code}")
+        response.raise_for_status()
     return response.json()
 
 
-if __name__ == '__main__':
-    name_pattern = re.compile(rf'{args.pattern}')
+if __name__ == "__main__":
+    name_pattern = re.compile(rf"{args.pattern}")
 
     queues = get_queues()
 
     for queue in queues:
-        if name_pattern.match(queue['name']) and queue['vhost'] == VHOST:
-            if config.getboolean('Actions', 'deletion_dry_run', fallback=True):
-                print(f"Would delete queue {queue['name']} on vhost {queue['vhost']}")
+        if name_pattern.match(queue["name"]) and queue["vhost"] == VHOST:
+            if config.getboolean("Actions", "deletion_dry_run", fallback=True):
+                print(
+                    f"Would delete queue {queue['name']} on vhost {queue['vhost']}"
+                )
                 continue
 
-            delete_status_code = delete_queue(queue)
-            if delete_status_code == 204:
-                print(f"Queue {queue['name']} deleted on vhost {queue['vhost']}")
+            status_code = delete_queue(queue)
+            if status_code == 204:
+                print(
+                    f"Queue {queue['name']} deleted on vhost {queue['vhost']}"
+                )
             else:
-                print(f"Error deleting queue {queue['name']} on vhost {queue['vhost']}: {delete_status_code}")
+                print(
+                    f"Error deleting queue {queue['name']} on vhost {queue['vhost']}: {status_code}"
+                )
